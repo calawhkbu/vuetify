0701d9e680e7892514c4a446594bf1645d2567f5
calawswive <calvin.law@swivelsoftware.com>
2020-08-20 10:52:07 +0800

    navigation drawer v-on:click="var=!var"

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,6 +1,9 @@
 <template>
 <div>
     <v-toolbar app dark color=" white"> 
+        <v-app-bar-nav-icon class="grey--text"
+        v-on:click="drawer=!drawer">
+        </v-app-bar-nav-icon>
 <v-toolbar-title class="text-uppercase grey--text">
 <span class="font-weight-light">Todo</span>
 <span>calaw</span>
@@ -12,10 +15,23 @@
 
 </v-btn>
     </v-toolbar>
+    <v-navigation-drawer 
+    v-model="drawer"
+    app
+    
+    class="indigo">
+        <p>test</p>
+
+    </v-navigation-drawer>
     </div>
 </template>
 <script>
 export default {
+    data(){
+        return{
+            drawer:false,
+        }
+    }
     
 }
 </script>
