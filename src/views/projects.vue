2b8e80f5c1188810b6f6a01d3031e10cdbc4b47d
calawswive <calvin.law@swivelsoftware.com>
2020-08-20 11:56:03 +0800

    grid system part 1

--- src/views/projects.vue
+++ src/views/projects.vue
@@ -3,7 +3,28 @@
     <h1 class="subheading grey--text">Projects</h1>
         <router-link to="/"><a>back</a></router-link>
 <v-container  class='my-5'>
-  Content
+  <v-layout row wrap>
+    <v-flex xs12 md6 >
+      <v-btn outline block class="primary">1</v-btn>
+    </v-flex>
+    <v-flex xs4 md6>
+       <v-btn outline block class="primary">2</v-btn>
+    </v-flex>
+  </v-layout>
+  <v-layout row wrap justify-space-around>
+    <v-flex xs4 md3>
+      <v-btn
+      outline block class="success">
+        1
+      </v-btn>
+           <v-btn
+      outline block class="success">
+        2
+      </v-btn>
+
+    </v-flex>
+
+  </v-layout>
 </v-container>
   </div>
 </template>
