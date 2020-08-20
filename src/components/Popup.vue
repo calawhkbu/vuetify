6dcc1699d730c018ff6bf52fad72f520b0524f1d
calawswive <calvin.law@swivelsoftware.com>
2020-08-20 23:52:25 +0800

    firebase and firestore using google noSQL

--- src/components/Popup.vue
+++ src/components/Popup.vue
@@ -9,49 +9,46 @@
         <v-card-title class="headline grey lighten-2">Create Project</v-card-title>
 
         <v-card-text>
-          <v-form class="px-3 " ref="form">
-          <v-text-field label="Title" v-model="title" prepend-icon="folder" :rules="inputRules"></v-text-field>
-          <v-textarea label="Information" v-model="information" prepend-icon="edit"></v-textarea>
-          <v-row>
-            <v-col cols="12" lg="6">
+          <v-form class="px-3" ref="form">
+            <v-text-field label="Title" v-model="title" prepend-icon="folder" :rules="inputRules"></v-text-field>
+            <v-textarea label="Information" v-model="information" prepend-icon="edit"></v-textarea>
+            <v-row>
+              <v-col cols="12" lg="6">
+                <v-menu
+                  ref="menu1"
+                  v-model="menu1"
+                  :close-on-content-click="false"
+                  transition="scale-transition"
+                  offset-y
+                  max-width="290px"
+                  min-width="290px"
+                >
+                  <template v-slot:activator="{ on, attrs }">
+                    <v-text-field
+                      v-model="date"
+                      label="Due Date"
+                      hint="MM/DD/YYYY format"
+                      persistent-hint
+                      prepend-icon="event"
+                      v-bind="attrs"
+                      @blur="date = parseDate(dateFormatted)"
+                      v-on="on"
+                    ></v-text-field>
+                  </template>
 
-              <v-menu
-                ref="menu1"
-                v-model="menu1"
-                :close-on-content-click="false"
-                transition="scale-transition"
-                offset-y
-                max-width="290px"
-                min-width="290px"
-                 
-              >
-                <template v-slot:activator="{ on, attrs }">
-                  <v-text-field
-                    v-model="date"
-                    label="Due Date"
-                    hint="MM/DD/YYYY format"
-                    persistent-hint
-                    prepend-icon="event"
-                    v-bind="attrs"
-                    @blur="date = parseDate(dateFormatted)"
-                    v-on="on"
-                  ></v-text-field>
-                </template>
+                  <v-date-picker v-model="date" no-title @input="menu1 = false"></v-date-picker>
+                </v-menu>
+                <p>
+                  Date in ISO format:
+                  <strong>{{ date }}</strong>
+                </p>
+                <div>moment Date YYYY:{{calDate}}</div>
+              </v-col>
+            </v-row>
 
-                <v-date-picker v-model="date" no-title @input="menu1 = false"></v-date-picker>
-              </v-menu>
-              <p>
-                Date in ISO format:
-                <strong>{{ date }}</strong>
-              </p>
-              <div>moment Date YYYY:{{calDate}}</div>
-            </v-col>
-          </v-row>
-
-          <v-btn flat class="success mx0 mt-3" @click="submit">Add Project</v-btn>
-         </v-form>
+            <v-btn flat class="success mx0 mt-3" @click="submit" :loading="created">Add Project</v-btn>
+          </v-form>
         </v-card-text>
-      
 
         <v-divider></v-divider>
 
@@ -64,7 +61,7 @@
 </template>
 <script>
 import Vue from "vue";
-import db from '@/fb'
+import db from "@/fb";
 
 const moment = require("moment");
 require("moment/locale/en-gb");
@@ -79,36 +76,33 @@ export default {
   },
   data() {
     return {
+      created: true,
       title: "",
       information: "",
       date: null,
-      inputRules:[
-        v=>v.length>=3 || 'Minimum Length is 3 characters'
-      ]
+      inputRules: [(v) => v.length >= 3 || "Minimum Length is 3 characters"],
     };
   },
   methods: {
-   async submit() {
-     if(this.$refs.form.validate()) {
-      console.log(this.title, this.information);
-      const project={
-        title:this.title,
-        content:this.information,
-        due:this.$moment(this.date).format("YYYY-MM-DD"),
-        status:"Open",
-        person:"The PopUP VUE"
-
-
+    submit() {
+      if (this.$refs.form.validate()) {
+        console.log(this.title, this.information);
+        const project = {
+          title: this.title,
+          content: this.information,
+          due: this.$moment(this.date).format("YYYY-MM-DD"),
+          status: "Open",
+          person: "The PopUP VUE",
+        };
+        db.collection("projects")
+          .add(project)
+          .then(() => (this.created = false));
       }
-      await db.collection('projects').add(project);
-
-
-     }
     },
   },
   computed: {
     calDate: function () {
-      return this.date?this.$moment(this.date).format("YYYY"):'';
+      return this.date ? this.$moment(this.date).format("YYYY") : "";
     },
   },
 };
