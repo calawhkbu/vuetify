06676b6eeedfcedae0d4bf4dd973504bef495e58
calawswive <calvin.law@swivelsoftware.com>
2020-08-20 14:03:58 +0800

    expandable row

--- src/views/projects.vue
+++ src/views/projects.vue
@@ -1,38 +1,46 @@
 <template>
   <div class="about">
     <h1 class="subheading grey--text">Projects</h1>
-        <router-link to="/"><a>back</a></router-link>
-<v-container  class='my-5'>
-  <v-layout row wrap>
-    <v-flex xs12 md6 >
-      <v-btn outline block class="primary">1</v-btn>
-    </v-flex>
-    <v-flex xs4 md6>
-       <v-btn outline block class="primary">2</v-btn>
-    </v-flex>
-  </v-layout>
-  <v-layout row wrap justify-space-around>
-    <v-flex xs4 md3>
-      <v-btn
-      outline block class="success">
-        1
-      </v-btn>
-           <v-btn
-      outline block class="success">
-        2
-      </v-btn>
+    <router-link to="/">
+      <a>back</a>
+    </router-link>
 
-    </v-flex>
-
-  </v-layout>
-</v-container>
+    <v-container class="my-5">
+    <v-expansion-panels>
+    <v-expansion-panel
+      v-for="i in myProjects"
+      :key="i.title"
+    >
+      <v-expansion-panel-header>{{i.title}}</v-expansion-panel-header>
+      <v-expansion-panel-content>
+        Due Date:
+{{i.due}}    
+  </v-expansion-panel-content>
+    </v-expansion-panel>
+  </v-expansion-panels>
+    </v-container>
   </div>
 </template>
 <script>
 export default {
-  
+  data() {
+    return {
+      projects: [
+        { title: 'Design a new website', person: 'The Net Ninja', due: '1st Jan 2019', status: 'ongoing', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
+        { title: 'Code up the homepage', person: 'Chun Li', due: '10th Jan 2019', status: 'complete', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
+        { title: 'Design video thumbnails', person: 'Ryu', due: '20th Dec 2018', status: 'complete', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
+        { title: 'Create a community forum', person: 'Gouken', due: '20th Oct 2018', status: 'overdue', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
+      ]
+    }
+  },
+  computed: {
+    myProjects() {
+      return this.projects.filter(project => {
+        return project.person === 'The Net Ninja' && project.status != 'complete'
+      })
+    }
+  }
 }
 </script>
 <style scoped>
-
 </style>
