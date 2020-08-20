f9c01d51a34e357f630b78a908f6e1fcf694ac68
calawswive <calvin.law@swivelsoftware.com>
2020-08-20 14:51:31 +0800

    popup bind text with data in vue

--- src/components/Popup.vue
+++ src/components/Popup.vue
@@ -1,42 +1,47 @@
 <template>
-<div class="text-center">
-    <v-dialog
-      v-model="dialog"
-      width="500"
-    >
+  <div class="text-center">
+    <v-dialog v-model="dialog" width="500">
       <template v-slot:activator="{ on, attrs }">
-        <v-btn
-          color="red lighten-2"
-          dark
-          v-bind="attrs"
-          v-on="on"
-        >
-          Create Project
-        </v-btn>
+        <v-btn color="red lighten-2" dark v-bind="attrs" v-on="on">Create Project</v-btn>
       </template>
 
       <v-card>
-        <v-card-title class="headline grey lighten-2">
-          Privacy Policy
-        </v-card-title>
+        <v-card-title class="headline grey lighten-2">Create Project</v-card-title>
 
         <v-card-text>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          <v-text-field label="Title" v-model="title"
+          prepend-icon="folder"></v-text-field>
+          <v-textarea label="Information"
+          v-model="information"
+          prepend-icon="edit"></v-textarea>
+          <v-btn flat class="success mx0 mt-3"
+          @click="submit"
+          >Add Project</v-btn>
         </v-card-text>
 
         <v-divider></v-divider>
 
         <v-card-actions>
           <v-spacer></v-spacer>
-          <v-btn
-            color="primary"
-            text
-            @click="dialog = false"
-          >
-            I accept
-          </v-btn>
         </v-card-actions>
       </v-card>
     </v-dialog>
   </div>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      title: "",
+      information: "",
+      date: null,
+    };
+  },
+  methods:{
+      submit(){
+
+console.log(this.title,this.information)
+      }
+  }
+};
+</script>
