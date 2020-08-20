b54bb998afd7116f47f220c4fb1026267789077c
calawswive <calvin.law@swivelsoftware.com>
2020-08-20 09:52:25 +0800

    text

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,7 +3,16 @@
     <img alt="Vue logo" src="../assets/logo.png">
     <h1>This is HomePage</h1>
     <router-link to="/about"><a>about</a></router-link>
+  <p class="pink lighten red --text text--darken 4">Lorem ipsum dolor sit amet consectetur, adipisicing elit. In explicabo numquam omnis at est, quibusdam dicta reprehenderit laudantium perspiciatis itaque porro sapiente eius consequatur aliquid provident dolorum, doloremque ratione accusantium.</p>
+  <h1 class="display-1">Massive Display</h1>
+    <h1 class="display-2">Massive Display</h1>
+  <h1 class="display-3">Massive Display</h1>
+    <h1 class="display-4">Massive Display</h1>
+    <p class=".text-uppercase"> This is a paragraph</p>
+
+
   </div>
+
 </template>
 
 <script>
