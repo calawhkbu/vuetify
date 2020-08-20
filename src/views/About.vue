34811ddf4e34407d711ad7e05ee2eff7da0e5fb0
calawswive <calvin.law@swivelsoftware.com>
2020-08-20 09:42:02 +0800

    vue add vuetify as plugin and vue add router

---
+++ src/views/About.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+        <router-link to="/"><a>back</a></router-link>
+
+  </div>
+</template>
---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <h1>This is HomePage</h1>
+    <router-link to="/about"><a>about</a></router-link>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'Home',
+  components: {
+    
+  }
+}
+</script>
